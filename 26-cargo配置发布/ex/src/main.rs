2d794884ab07118815843568c7e55747cf8c808a
lei <81576870@qq.com>
2023-01-03 21:42:07 +0800

    cargo配置与发布

---
+++ 26-cargo配置发布/ex/src/main.rs
@@ -0,0 +1,9 @@
+// use ex::kinds::PrimaryColor;
+// use ex::utils::mix;
+use ex::PrimaryColor;
+use ex::mix;
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
